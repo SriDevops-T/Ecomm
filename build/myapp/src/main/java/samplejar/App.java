28905b02d1575ed916c59eb66f8aa1ad0c636197
srinivas <srini2devops@gmail.com>
2020-12-02 05:04:42 +0000

    this is my firstcommit on build project

---
+++ build/myapp/src/main/java/samplejar/App.java
@@ -0,0 +1,13 @@
+package samplejar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
